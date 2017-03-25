8d9f89e94c4bc41ca424eabeade7f29467e46038
Alon Fliess <alonf@codevalue.net>
2017-03-25 13:39:37 +0300

    BasicGateApp - Single Button in XAML

---
+++ BasicGateApp/BasicGateApp/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Azure.Devices.Client;
+using Xamarin.Forms;
+using Message = Microsoft.Azure.Devices.Client.Message;
+
+namespace BasicGateApp
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
