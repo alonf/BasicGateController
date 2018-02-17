6f1c1eb8845798923a1485ac3e317103345269fa
Alon Fliess <alon.fliess@gmail.com>
2018-02-17 21:24:56 +0200

    Add MQTT support

--- MiniatureGate/AzureIoTHubHttpClient.cpp
+++ MiniatureGate/AzureIoTHubHttpClient.cpp
@@ -5,25 +5,27 @@
 #include "AzureIoTHubManager.h"
 #include "azure_c_shared_utility\platform.h"
 
+char  const *__ctype_ptr__ = 0;
+
 void sendCallback(IOTHUB_CLIENT_CONFIRMATION_RESULT result, void* userContextCallback)
 {
 	int messageTrackingId = reinterpret_cast<intptr_t>(userContextCallback);
 
-	LogInfo("Message Id: %d Received.\r\n", messageTrackingId);
+	printf_P("Message Id: %d Received.\r\n", messageTrackingId);
 
-	LogInfo("Result Call Back Called! Result is: %s \r\n", ENUM_TO_STRING(IOTHUB_CLIENT_CONFIRMATION_RESULT, result));
+	printf_P("Result Call Back Called! Result is: %s \r\n", ENUM_TO_STRING(IOTHUB_CLIENT_CONFIRMATION_RESULT, result));
 }
 
 
 static IOTHUBMESSAGE_DISPOSITION_RESULT IoTHubMessage(IOTHUB_MESSAGE_HANDLE message, void* userContextCallback)
 {
-	LogInfo("IoTHubMessage\n");
+	printf_P("IoTHubMessage\n");
 	IOTHUBMESSAGE_DISPOSITION_RESULT result;
 	const unsigned char* buffer;
 	size_t size;
 	if (IoTHubMessage_GetByteArray(message, &buffer, &size) != IOTHUB_MESSAGE_OK)
 	{
-		LogInfo("unable to IoTHubMessage_GetByteArray\r\n");
+		printf_P("unable to IoTHubMessage_GetByteArray\r\n");
 		return IOTHUBMESSAGE_REJECTED;
 	}// else
 	char commandBuffer[16]; //command should be less then 15 characters
@@ -35,7 +37,7 @@ static IOTHUBMESSAGE_DISPOSITION_RESULT IoTHubMessage(IOTHUB_MESSAGE_HANDLE mess
 	commandBuffer[size] = 0;
 	String command(commandBuffer);
 	
-	Serial.printf("Message arrived, command:%s\n", command.c_str());
+	printf_P("Message arrived, command:%s\n", command.c_str());
 	AzureIoTHubManager::Instance()->HandleCommand(command);
 			
 	return IOTHUBMESSAGE_ACCEPTED;
@@ -48,32 +50,41 @@ extern "C" bool AzureIoTHubInit(const char *connectionString)
 	Serial.printf("Connection string: %s\n", connectionString);
 	if (platform_init() != 0)
 	{
-		printf("Failed to initialize the platform.\r\n");
+		printf_P("Failed init platform.\n");
 		return false;
 	}
-	iotHubClientHandle = IoTHubClient_LL_CreateFromConnectionString(connectionString, HTTP_Protocol);
-	srand(static_cast<unsigned int>(time(nullptr)));
+	for (int i = 0; i < 5; ++i) //try to use MQTT 5 times, and then fall to HTTP
+	{
+		iotHubClientHandle = IoTHubClient_LL_CreateFromConnectionString(connectionString, MQTT_Protocol);
+		if (iotHubClientHandle != nullptr)
+			break;
+	
+		printf_P("Failed create with MQTT try #%d\n", i);
+	}
 
 	if (iotHubClientHandle == nullptr)
 	{
-		LogInfo("Failed on IoTHubClient_LL_Create\r\n");
-		return false;
+		iotHubClientHandle = IoTHubClient_LL_CreateFromConnectionString(connectionString, HTTP_Protocol);
+		if (iotHubClientHandle == nullptr)
+		{
+			printf_P("Failed create with HTTP\n");
+			return false;
+		}
+		//HTTP requires polling
+		unsigned int minimumPollingTime = 9; /*because it can poll "after 9 seconds" polls will happen effectively at 4 seconds*/
+		if (IoTHubClient_LL_SetOption(iotHubClientHandle, "MinimumPollingTime", &minimumPollingTime) != IOTHUB_CLIENT_OK)
+		{
+			printf_P("failure in \"MinimumPollingTime\"\n");
+			return false;
+		}
 	}
-
-
-	unsigned int minimumPollingTime = 9; /*because it can poll "after 9 seconds" polls will happen effectively at 4 seconds*/
-	if (IoTHubClient_LL_SetOption(iotHubClientHandle, "MinimumPollingTime", &minimumPollingTime) != IOTHUB_CLIENT_OK)
-	{
-		LogInfo("failure to set option \"MinimumPollingTime\"\r\n");
-		return false;
-	}
-
+	
 	if (IoTHubClient_LL_SetMessageCallback(iotHubClientHandle, IoTHubMessage, nullptr) != IOTHUB_CLIENT_OK)
 	{
-		LogInfo("unable to IoTHubClient_SetMessageCallback\r\n");
+		printf_P("unable to SetMessageCallback\n");
 		return false;
 	}
-	LogInfo("IoTHubClient initiated\r\n");
+	printf_P("IoTHubClient initiated\n");
 	return true;
 }
 
@@ -84,17 +95,17 @@ extern "C" bool AzureIoTHubSendMessage(const char *deviceId, const char *status)
 	IOTHUB_MESSAGE_HANDLE messageHandle = IoTHubMessage_CreateFromString(status);
 	if (messageHandle == nullptr)
 	{
-		LogInfo("unable to create a new IoTHubMessage\r\n");
+		printf_P("unable to create a new IoTHubMessage\r\n");
 	}
 	else
 	{
 		if (IoTHubClient_LL_SendEventAsync(iotHubClientHandle, messageHandle, sendCallback, reinterpret_cast<void*>(1)) != IOTHUB_CLIENT_OK)
 		{
-			LogInfo("failed to hand over the message to IoTHubClient");
+			printf_P("failed to hand over the message to IoTHubClient");
 		}
 		else
 		{
-			LogInfo("IoTHubClient accepted the message for delivery\r\n");
+			printf_P("IoTHubClient accepted the message for delivery\r\n");
 			result = true;
 		}
 
@@ -103,37 +114,13 @@ extern "C" bool AzureIoTHubSendMessage(const char *deviceId, const char *status)
 	return result;
 }
 
-
 extern "C" void AzureIoTHubLoop(void)
 {
 	static unsigned int t = 0;
 	if (++t % 100000 == 0)
-		Serial.write(".");
+		printf_P(".");
 	IoTHubClient_LL_DoWork(iotHubClientHandle);
 }
 
 
-//static void sendMessage(IOTHUB_CLIENT_LL_HANDLE iotHubClientHandle, const unsigned char* buffer, size_t size)
-//{
-//	static unsigned int messageTrackingId;
-//	IOTHUB_MESSAGE_HANDLE messageHandle = IoTHubMessage_CreateFromByteArray(buffer, size);
-//	if (messageHandle == nullptr)
-//	{
-//		LogInfo("unable to create a new IoTHubMessage\r\n");
-//	}
-//	else
-//	{
-//		if (IoTHubClient_LL_SendEventAsync(iotHubClientHandle, messageHandle, sendCallback,
-//			reinterpret_cast<void*>(static_cast<uintptr_t>(messageTrackingId))) != IOTHUB_CLIENT_OK)
-//		{
-//			LogInfo("failed to hand over the message to IoTHubClient");
-//		}
-//		else
-//		{
-//			LogInfo("IoTHubClient accepted the message for delivery\r\n");
-//		}
-//		IoTHubMessage_Destroy(messageHandle);
-//	}
-//	free(const_cast<unsigned char*>(buffer));
-//	messageTrackingId++;
-//}
+
